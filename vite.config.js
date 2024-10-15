21ce0241b9bd935f60549b408393b95744e0d9d7
ksriramreddy <k.sriramreddy9@gmail.com>
2024-10-15 09:29:18 +0530

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' 
+import from 'vite-plugin-vercel'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port:process.env.PORT 
+  },
   plugins: [react()],
 })
